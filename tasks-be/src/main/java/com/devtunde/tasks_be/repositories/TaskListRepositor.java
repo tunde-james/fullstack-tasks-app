4bf4d2141711bdb1d44dd045a1cd0c4d4bae6a91
Tunde James <59791126+tunde-james@users.noreply.github.com>
2025-09-02 15:53:09 +0100

    Create Task and TaskList repositories

---
+++ tasks-be/src/main/java/com/devtunde/tasks_be/repositories/TaskListRepositor.java
@@ -0,0 +1,13 @@
+package com.devtunde.tasks_be.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devtunde.tasks_be.domain.entities.TaskList;
+
+@Repository
+public interface TaskListRepositor extends JpaRepository<TaskList, UUID> {
+
+}
---
+++ tasks-be/src/main/java/com/devtunde/tasks_be/repositories/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.devtunde.tasks_be.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devtunde.tasks_be.domain.entities.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findByTaskListId(UUID taskListId);
+
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+}
