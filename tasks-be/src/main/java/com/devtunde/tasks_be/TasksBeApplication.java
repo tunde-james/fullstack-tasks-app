5968d49279ba1e8cc4d0d338602404bdea4440d8
Tunde James <59791126+tunde-james@users.noreply.github.com>
2025-09-02 13:49:56 +0100

    Add the frontend code Setup the backend project Create the Task and TaskList entities

---
+++ tasks-be/src/main/java/com/devtunde/tasks_be/TasksBeApplication.java
@@ -0,0 +1,13 @@
+package com.devtunde.tasks_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksBeApplication.class, args);
+	}
+
+}
---
+++ tasks-be/src/main/java/com/devtunde/tasks_be/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.devtunde.tasks_be.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSED
+}
